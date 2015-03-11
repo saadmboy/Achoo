46550b2990a339fbed1475d8f2cce637c4d5b109
Saad Kothawala <skothawala@luc.edu>
2015-03-10 22:31:31 -0500

    replace json with txt

--- Scraper/Scrape.py
+++ Scraper/Scrape.py
@@ -4,19 +4,19 @@ import json
 import time
 #import lxml #http://lxml.de/ Used to parse the html
 from lxml import html
+import sys
+reload(sys)  
+sys.setdefaultencoding('Cp1252')
 
 #@author Saad Kothawala
 
 DISEASE_LIST_LINK_BASE = "http://www.mayoclinic.org/diseases-conditions/index?letter="
 MAIN_LINK = "http://www.mayoclinic.org"
-FILE_NAME = "diseases.json"
+FILE_NAME = "diseases.txt"
 
 print ("--------------Starting Mayo Clinic Scraper--------------")
 
-data = {}
 startTime = time.time()
-data['lastUpdated'] = startTime
-
 
 diseasesAdded = []
 
@@ -29,6 +29,7 @@ for letter in string.ascii_lowercase:#Go through all the alphabet
 	for l in links:
 		diseaseName = l.text
 		if diseaseName[0].lower() == letter and diseaseName not in diseasesAdded:#to avoid duplicates
+			toWrite = diseaseName + "\t";
 			count += 1
 			print count, ": ", diseaseName
 			diseaseLink = MAIN_LINK + l.attrib['href'].replace('definition', 'symptoms')
@@ -36,17 +37,24 @@ for letter in string.ascii_lowercase:#Go through all the alphabet
 			diseasePage = urllib2.urlopen(diseaseLink).read()
 			diseaseTree = html.fromstring(diseasePage)
 			symptoms = diseaseTree.xpath('//*[@id="main-content"]/ul[1]/li')
-			symptomsJSON = []
+			
+			flag = 0
 			for symptom in symptoms:
-				if not "Tweet" in symptom.text_content() and not "Share" in symptom.text_content():
-					symptomsJSON.append(symptom.text_content().replace("\r\n", ""))
+				if not "Tweet" in symptom.text_content() and not "Share" in symptom.text_content() and not "Definition" in symptom.text_content() and not "Causes" in symptom.text_content():
+					toWrite += (symptom.text_content().replace("\r\n", "") + "\t").encode("utf-8")
+					flag = 1
+					diseasesAdded.append(diseaseName)
 
-			data[diseaseName] = symptomsJSON
-			diseasesAdded.append(diseaseName)
 
-obj = open(FILE_NAME, 'wb')
-obj.write(json.dumps(data))
-obj.close
+
+			toWrite += "\n";
+			toWrite.encode('utf-8')
+			if flag is 1:#write only if symptopms present
+				obj = open(FILE_NAME, 'ab')
+				obj.write(toWrite)
+				obj.close
+
+
 			
 
 print ("--------------Done Running Mayo Clinic Scraper--------------\n")
